e9d896ed1001393132c0051e4ad98e26a5e6fd35
David Wang <weihuaw@gmail.com>
2014-03-28 16:40:17 +1100

    Update ShortestDistance.cpp

--- ShortestDistance.cpp
+++ ShortestDistance.cpp
@@ -23,3 +23,53 @@ int ShortestDistance(string text[], int n, string word1, string word2){
     }
     return min_dist;
 }
+
+// a better way
+int ShortestDist(string text[], int n, string word1, string word2)
+{
+
+    unordered_map<string, vector<int> > map_word_indexs;
+
+    InitMap(text, n, map_word_indexs);
+
+    if( map_word_indexs.find(word1) == map_word_indexs.end()
+
+        || map_word_indexs.find(word2) == map_word_indexs.end())
+
+    return INT_MAX;
+
+    
+
+    vector<int>& v1 = map_word_indexs[word1];
+
+    vector<int>& v2 = map_word_indexs[word2];
+
+    vector< pair<int, int> > vsorted;
+
+    for(int i = 0; i < v1.size(); ++i)
+
+        vsorted.push( make_pair(v1[i], 0) );
+
+    for(int i = 0; i < v2.size(); ++i)
+
+        vsorted.push( make_pair(v2[i], 1) );
+
+    sort(vsorted.begin(), vsorted.end());
+
+    
+
+    int min_dist = INT_MAX;
+
+    for(int i = 1; i < vsorted.size(); ++i)
+
+    {
+
+        if(vsorted[i-1].second != vsorted[i].second)
+
+            min_dist  = min(min_dist, vsorted[i].first - vsorted[i-1].first);
+
+    }
+
+    return min_dist;
+
+}
